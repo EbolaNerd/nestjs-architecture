fa836d462ff8fca84f68239ad7438800b8616f3a
Lars Wolter <lwolter1337@gmail.com>
2024-03-27 16:37:56 +0100

    Almost done with hexagon part 2. Shit doesn't work

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AlarmsService } from '../../application/alarms.service';
+import { CreateAlarmCommand } from '../../application/commands/create-alarm.command';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
-import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
 
 @Controller('alarms')
 export class AlarmsController {
@@ -9,9 +9,8 @@ export class AlarmsController {
 
   @Post()
   create(@Body() createAlarmDto: CreateAlarmDto) {
-
     return this.alarmsService.create(
-      new CreateAlarmCommand(createAlarmDto.name, createAlarmDto.severity)
+      new CreateAlarmCommand(createAlarmDto.name, createAlarmDto.severity),
     );
   }
 
